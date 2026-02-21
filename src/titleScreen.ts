a9dc9a74f0b77a80518bc0e39d6b2413993f0a4d
James Randall <james@accidentalfish.com>
2026-02-21 09:22:48 +0000

    Tweaks to title

--- src/titleScreen.ts
+++ src/titleScreen.ts
@@ -20,17 +20,17 @@ const w = bbcMicroColours.white;
 const titlePages: TitleEntry[][] = [
   [
     { row: 4, text: "THRUST", color: bbcMicroColours.green },
-    { row: 6, text: "Rotate left: {A}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
-    { row: 7, text: "Rotate right: {D}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
-    { row: 8, text: "Thrust: {W}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
+    { row: 6, text: "Thrust: {W}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
+    { row: 7, text: "Rotate left: {A}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
+    { row: 8, text: "Rotate right: {D}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
     { row: 9, text: "Fire: {RETURN}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
     { row: 10, text: "Shield / Tractor beam: {SPACE}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
     { row: 11, text: "Cycle effects: {[ ]}", color: bbcMicroColours.red, color2: bbcMicroColours.yellow },
 
     { row: 13, text: "Original game copyright", color: bbcMicroColours.magenta },
-    { row: 14, text: "Jeremy C Smith 1986", color: bbcMicroColours.magenta },
+    { row: 14, text: "{Jeremy C Smith} 1986", color: bbcMicroColours.magenta, color2: bbcMicroColours.yellow },
     { row: 16, text: "Recreated in Typescript by", color: bbcMicroColours.magenta },
-    { row: 17, text: "James Randall in 2026", color: bbcMicroColours.magenta },
+    { row: 17, text: "{James Randall} in 2026", color: bbcMicroColours.magenta, color2: bbcMicroColours.yellow },
     { row: 18, text: "https://jamesdrandall.com/", color: bbcMicroColours.yellow },
 
     { row: 21, text: "PRESS ANY KEY TO START", color: w },
